f378cd4c77c8f1420279e97407659c27a3c78128
Shelby Huston <Shelby Huston@SatansEyepiece>
2016-11-13 22:36:07 -0700

    wowza

--- Heap/src/PriorityQueue.java
+++ Heap/src/PriorityQueue.java
@@ -1,25 +1,51 @@
 import java.util.Comparator;
 
-public class PriorityQueue<E> implements RMCPriorityQueue<E>{
+public class PriorityQueue<E> implements RMCPriorityQueue<E> {
 
 	private E[] data;
-	private int size=0;
+	private int size = 0;
 	private Comparator<E> comparator;
-	
+
 	public PriorityQueue(Comparator<E> comparator, int start) {
-		this.data=(E[]) new Object[start];
-		// TODO Auto-generated constructor stub
+		this.data = (E[]) new Object[start];
+		this.comparator = comparator;
 	}
-	
-	public PriorityQueue(Comparator<E> comparator){
+
+	public PriorityQueue(Comparator<E> comparator) {
 		this(comparator, 23);
 	}
-	
-	private void percolateUp(int i){
-		//code stuff;
+
+	private void percolateUp(int i) {
+		if (data [parentOf(i)] != null) {
+			if (comparator.compare(data[i], data[parentOf(i)]) > 0) {
+				swap(i, parentOf(i));
+				percolateUp(parentOf(i));
+			}
+		}
 	}
-	
-	
+
+	public void percolateDown(int i) {
+		if (rightChildOf(i) >= size) {
+			if (comparator.compare(data[i], data[rightChildOf(i)]) < 0) {
+				swap(i, rightChildOf(i));
+			}
+		}
+		if (!(leftChildOf(i) >= size) && !(rightChildOf(i) >= size)) {
+			if (comparator.compare(data[leftChildOf(i)], data[rightChildOf(i)]) > 0) {
+				if (comparator.compare(data[i], data[leftChildOf(i)]) < 0) {
+					swap(i, (leftChildOf(i)));
+					percolateDown(leftChildOf(i));
+				}
+			} else {
+				if (comparator.compare(data[i], data[rightChildOf(i)]) < 0) {
+					swap(i, rightChildOf(i));
+					percolateDown(rightChildOf(i));
+				}
+			}
+		}
+
+	}
+
 	@Override
 	public E peek() {
 		return data[0];
@@ -27,32 +53,73 @@ public class PriorityQueue<E> implements RMCPriorityQueue<E>{
 
 	@Override
 	public E poll() {
-		E temp=peek();
-		data[0]=data[size];
-		size=size-1;
+		E temp = peek();
+		data[0] = data[size];
+		size = size - 1;
 		return temp;
 	}
 
 	@Override
 	public void add(E elt) {
-		if (data.length == size){
-			E[] temp;
-			temp =(E[]) new Object[data.length*2];
-			
-			for(int x=0;x<data.length;x++){
-				temp[x]=data[x];
-			}
-			data=temp;	
+		
+		if (data.length == size) {
+			growArray();
 		}
+
+		data[size] = elt;
 		
-		
-		
+		percolateUp(size);
+		size = size + 1;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return size;
+	}
+
+	public int leftChildOf(int i) {
+		return (2 * i) + 1;
+	}
+
+	public int rightChildOf(int i) {
+		return (2 * i) + 2;
+	}
+
+	public int parentOf(int i) {
+		return (i - 1) / 2;
+	}
+
+	public void swap(int a, int b) {
+		E temp = data[a];
+		data[a] = data[b];
+		data[b] = temp;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void growArray() {
+		E[] temp;
+		temp = (E[]) new Object[data.length * 2];
+
+		for (int x = 0; x < data.length; x++) {
+			temp[x] = data[x];
+		}
+		data = temp;
+	}
+
+	public void display() {
+		for(int x=0;x<size;x++){
+			System.out.println(data[x]);
+		}
+	}
+
+	public static void main(String[] args) {
+		PriorityQueue<Integer> john = new PriorityQueue<Integer>(new IntegerComparator(), 50);
+		for (int x = 0; x < 40; x++) {
+			john.add((int) Math.floor(100 * Math.random()));
+		}
+		john.display();
+
 	}
 
 }
